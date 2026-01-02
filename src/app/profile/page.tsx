6cbc9e204f2c376a8b11f9b4d2fcfbfb3aeee570
gladiatorr22 <likhitnataraj@gmail.com>
2026-01-02 12:55:56 +0530

    feat: Implement user profile page with an API to fetch authenticated user data from a token.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,12 @@ import { NextResponse } from "next/server";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { useState , useEffect} from "react";
+import Link from "next/link";
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [data, setData] = useState("nothing")
     const handleLogout = async () => {
         try {
             axios.get("/api/users/logout")
@@ -15,13 +18,36 @@ export default function ProfilePage() {
             return toast.error(error.response.data.message)
         }
     }
+    const fetchUser = async () => {
+        try {
+            const response = await axios.get("/api/users/me")
+            setData(response.data.data._id)
+            console.log(response.data.data._id)
+        } catch (error: any) {
+            console.log(error)
+        }
+    }
+   
+
     return (
         <div className="flex flex-col justify-center items-center w-full h-screen bg-black gap-4">
             <h1 className="h1 text-2xl text-center text-white">profile page</h1>
-        <hr/>
-        <button onClick={handleLogout} className="bg-red-500 text-white p-2 rounded hover:bg-red-600 transition">
-            Logout
-        </button>
+            <h2>{data==="nothing"?"nothing":
+                <Link className="text-white bg-blue-500 p-2 rounded hover:bg-blue-600 transition"
+                href={`/profile/${data}`}>
+                {data}
+                </Link>}
+            </h2>
+            <hr/>
+
+            <button onClick={handleLogout} className="bg-red-500 text-white p-2 rounded hover:bg-red-600 transition">
+                Logout
+            </button>
+
+            <button onClick={fetchUser} className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition">
+                Fetch User
+            </button>
+
         </div>
            
     )
